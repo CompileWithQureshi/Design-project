d366cc255d75b9248c17a226e8ec51901bbf2129
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-20 10:01:26 -0800

    Card-compeleted

--- src/components/cardfolder/cardContainer.jsx
+++ src/components/cardfolder/cardContainer.jsx
@@ -1,15 +1,29 @@
 import "./Card.css";
 import React from "react";
-import { AiOutlineUser } from "react-icons/ai";
+// import { AiOutlineUser } from "react-icons/ai";
+import { a } from "./data";
 
 function CardContainer() {
-  return (
-    <section>
-      <div className="cards">
-        <div className="card">
-          <AiOutlineUser />
+  console.log(a);
+  const cardDetails = a.map((items) => {
+    const { id, img, speacilist, name, rating } = items;
+    return (
+      <div key={id} className="card-details">
+        <div className="card-img">
+          <img src={img} alt="dr.img" width="130px" height="max-content" />
+        </div>
+        <div className="card-info">
+          <h5>{name}</h5>
+          <h5>{speacilist}</h5>
+          <p>{rating}</p>
         </div>
       </div>
+    );
+  });
+  return (
+    <section>
+      <h2>Top Doctors</h2>
+      <div className="cards">{cardDetails}</div>
     </section>
   );
 }
