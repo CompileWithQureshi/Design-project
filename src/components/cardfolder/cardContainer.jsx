6cb973ebaef7f743d89f31ea8347769b42f1e71f
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-20 08:45:18 -0800

    Data file

--- src/components/cardfolder/cardContainer.jsx
+++ src/components/cardfolder/cardContainer.jsx
@@ -5,30 +5,9 @@ import { AiOutlineUser } from "react-icons/ai";
 function CardContainer() {
   return (
     <section>
-      <div className="card">
-        <div className="card-img">
+      <div className="cards">
+        <div className="card">
           <AiOutlineUser />
-        </div>
-        <div className="card-info">
-          <h2>Nmae</h2>
-          <p>Speacilist</p>
-          <h4>* 5.0</h4>
-        </div>
-        <div className="card-img">
-          <AiOutlineUser />
-        </div>
-        <div className="card-info">
-          <h2>Nmae</h2>
-          <p>Speacilist</p>
-          <h4>* 5.0</h4>
-        </div>
-        <div className="card-img">
-          <AiOutlineUser />
-        </div>
-        <div className="card-info">
-          <h2>Nmae</h2>
-          <p>Speacilist</p>
-          <h4>* 5.0</h4>
         </div>
       </div>
     </section>
---
+++ src/components/cardfolder/data.jsx
@@ -0,0 +1,9 @@
+export const a = [
+  {
+    id: 1,
+    img: "./original.webp",
+    name: "Test1",
+    speacilist: "speacilist",
+    rating: 5.0,
+  },
+];
