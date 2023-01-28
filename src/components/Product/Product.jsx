ea6b9dd829bb27f6c4090d834572e7c90bde2829
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-28 09:55:24 -0800

    Seprating product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 import productData from "./productdata";
 function Product() {
-  const [proDetails, setProDetails] = useState(1);
-  const details = productData.slice(0, proDetails);
+  // const [proDetails, setProDetails] = useState(1);
+  const details = productData.slice(0, 1);
 
   const products = details.map((product) => {
     const { id, produ, price, Avalable } = product;
