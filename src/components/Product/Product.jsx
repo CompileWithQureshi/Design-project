3b76074800fa75692b3d430e08a06842d9bce7ff
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-28 10:16:59 -0800

    Done with mapping product data

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./product.css";
 import productData from "./productdata";
 function Product() {
-  // const [proDetails, setProDetails] = useState(1);
-  const details = productData.slice(0, 1);
+  const [proDetails, setProDetails] = useState(2);
+  const details = productData.slice(0, proDetails);
 
   const products = details.map((product) => {
     const { id, produ, price, Avalable } = product;
 
     console.log(product);
     return (
-      <div key={id}>
-        <p>Product Name: {produ}</p>
-        <p>Price: {price}</p>
-        <p>Avalaible: {Avalable}</p>
+      <div key={id} className="product-details">
+        <p className="product name">Product Name: {produ}</p>
+        <p className="product price">Price: {price}</p>
+        <p className="product Avalaible">Avalaible: {Avalable}</p>
       </div>
     );
   });
-  return <div className="product-container">{products}</div>;
+  return (
+    <div className="product-container">
+      <section>{products}</section>
+    </div>
+  );
 }
 
 export default Product;
